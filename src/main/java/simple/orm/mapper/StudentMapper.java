11fd4a9ea305029a94fba7d89bd81a06cf469008
wuguang3 <wuguang3@asiainfo.com>
2019-03-01 15:45:19 +0800

    升级为spring-boot框架，并整合mybatis完成generator自动生成代码

---
+++ src/main/java/simple/orm/mapper/StudentMapper.java
@@ -0,0 +1,17 @@
+package simple.orm.mapper;
+
+import simple.orm.model.Student;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(Integer studentId);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    Student selectByPrimaryKey(Integer studentId);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
---
+++ src/main/java/simple/orm/model/Student.java
@@ -0,0 +1,45 @@
+package simple.orm.model;
+
+import java.util.Date;
+
+public class Student {
+    private Integer studentId;
+
+    private String name;
+
+    private Integer age;
+
+    private Date birthday;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
